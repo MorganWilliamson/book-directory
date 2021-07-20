2bb205d7fc6d7f48f77dec0e35b40d1dea1c2de4
Morgan Williamson <morgantaylorwilliamson@protonmail.com>
2021-07-20 15:22:39 -0500

    Building out BookCard and BookList components.

--- src/Books/BookCard.js
+++ src/Books/BookCard.js
@@ -1,3 +1,14 @@
-// 1) Take data from Book.js as props
-// 2) Fill in data to generate card
-// 3) Return individual cards
+import React from "React";
+
+export default BookCard = (props) => {
+    const {book_name, author, onClick} = props;
+
+    return (
+        <div className="book-card" onClick={onClick}>
+            <h2>{book_name}</h2>
+            <div className="book-author">
+                Author: <p>{author}</p>
+            </div>
+        </div>
+    );
+}
--- src/Books/BookList.js
+++ src/Books/BookList.js
@@ -1,2 +1,22 @@
-// 1) Map cards to generate list
-// 2) Secondary function: Allow clickthrough to individual book card
+import React from "react";
+import { useHistory } from "react-router-dom";
+import BookCard from "./BookCard.js";
+
+export default BookList = (props) => {
+    return (
+        <div className="book-list">
+            {props.books.map(book => {
+                <BookDetails key={book.id} book={book} />
+            })}
+        </div>
+    );
+}
+
+function BookDetails(props) {
+    const history = useHistory();
+    const routeDescription = () => {
+        history.push(`/books/${props.book.id}`);
+    };
+
+    return <BookCard {...props.book} onClick={routeDescription} />;
+}
